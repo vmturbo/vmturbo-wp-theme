65eed60ea8d4348fcf18d363cd1623c907115fc8
alinaw89 <ali.naw89@gmail.com>
2016-05-06 13:52:28 -0400

    updated picsand removed unused code

--- template-public-cloud-guide.php
+++ template-public-cloud-guide.php
@@ -5,62 +5,8 @@
 ?>
 <style>
 body {
-    background: #222 url('http://localhost:8888/vmturbo/wp-content/uploads/2016/05/plex-dark.jpg') no-repeat center center;
-    background-size:cover;
-  }
-  #home-jumbo-sub {
-    text-align:center;
-    padding:100px 0;
-    position:relative;
-    overflow-x:hidden;
-  }
-      #home-jumbo-sub h3 {
-        margin-top:0;
-        font-weight:700;
-      }
-      #home-jumbo-sub p {
-        line-height: 1.8em;
-        margin-bottom:0;
-      }
-      #home-jumbo-sub a:last-child {
-        font-weight: 600;
-        font-size: 1em;
-        margin-top: 1em;
-        display:inline-block;
-      }
-  #home-jumbo {
-    text-align:center;
-    background: #444 url('http://cdn.vmturbo.com/wp-content/uploads/2016/03/home-header-bg2b.jpg') no-repeat center center fixed;
-    background-size: cover;
-    padding: 80px 0 0 0;
-    color: #fff;
-
-  }
-    #home-jumbo a {
-      color:#fff;
-      text-decoration:none;
-    }
-    #home-jumbo img {
-      position:relative;
-      z-index:1;
-    }
-    #home-jumbo h1 {
-      font-weight:700;
-      margin-top:0;
-    }
-    #home-jumbo span {
-      font-weight:600;
-    }
-
-  #submit-form {
-    margin-top:3em;
-  }
-  #sniper-content {
-    color:#fff;
-  }
-  #sniper-content h3 {
-    font-weight:600;
-    margin-top:0;
+  background: #222 url('http://cdn.vmturbo.com/wp-content/uploads/2016/05/plex-dark.jpg') no-repeat center center;
+  background-size:cover;
   }
 #home-multi {
   padding:3em 1.5em;
@@ -75,10 +21,9 @@ body {
     margin-top: -3.2em !important;
   }
 }
-
 #home-multi p {
-    color:#eee;
-    line-height:1.6em;
+  color:#eee;
+  line-height:1.6em;
 }
 #home-multi img {
   margin: 0 auto;
@@ -113,162 +58,22 @@ li {
   list-style-type: disc;
   margin-left: -1.5em;
 }
-
 </style>
 
 <div id="home-multi" class="container">
-
   <div class="row">
     <div class="col-sm-12">
-      <img src="http://localhost:8888/vmturbo/wp-content/uploads/2016/05/pcf-guide.png" class="img-responsive" alt="Public Cloud Guide" title="Public Cloud Guide" style="margin: 0 auto;" />
+      <img src="http://cdn.vmturbo.com/wp-content/uploads/2016/05/pcf-guide.png" class="img-responsive" alt="Public Cloud Guide" title="Public Cloud Guide" style="margin: 0 auto;" />
     </div>
   </div>
   <div class="row overall-content">
     <div class="col-sm-6">
-      <img src="http://localhost:8888/vmturbo/wp-content/uploads/2016/05/853-ipad-resized-new-cover.png" class="img-responsive hidden-xs" />
+      <img src="http://cdn.vmturbo.com/wp-content/uploads/2016/05/public-cloud-guide-ipad.png" class="img-responsive hidden-xs" />
     </div>
     <div class="col-sm-6">
-
-    <div class="public-cloud-content">
-    <?php the_content(); ?>
-    </div>
-    </div>
-  </div>
-</div>
-<?php
-/**
- * Template Name: Temporary: Public Cloud Guide
- */
-?>
-<style>
-body {
-    background: #222 url('http://localhost:8888/vmturbo/wp-content/uploads/2016/05/plex-dark.jpg') no-repeat center center;
-    background-size:cover;
-  }
-  #home-jumbo-sub {
-    text-align:center;
-    padding:100px 0;
-    position:relative;
-    overflow-x:hidden;
-  }
-      #home-jumbo-sub h3 {
-        margin-top:0;
-        font-weight:700;
-      }
-      #home-jumbo-sub p {
-        line-height: 1.8em;
-        margin-bottom:0;
-      }
-      #home-jumbo-sub a:last-child {
-        font-weight: 600;
-        font-size: 1em;
-        margin-top: 1em;
-        display:inline-block;
-      }
-  #home-jumbo {
-    text-align:center;
-    background: #444 url('http://cdn.vmturbo.com/wp-content/uploads/2016/03/home-header-bg2b.jpg') no-repeat center center fixed;
-    background-size: cover;
-    padding: 80px 0 0 0;
-    color: #fff;
-
-  }
-    #home-jumbo a {
-      color:#fff;
-      text-decoration:none;
-    }
-    #home-jumbo img {
-      position:relative;
-      z-index:1;
-    }
-    #home-jumbo h1 {
-      font-weight:700;
-      margin-top:0;
-    }
-    #home-jumbo span {
-      font-weight:600;
-    }
-
-  #submit-form {
-    margin-top:3em;
-  }
-  #sniper-content {
-    color:#fff;
-  }
-  #sniper-content h3 {
-    font-weight:600;
-    margin-top:0;
-  }
-#home-multi {
-  padding:3em 1.5em;
-}
-@media all and (max-width: 480px) {
-  #home-multi {
-    padding:1em 1.5em;
-  }
-}
-@media all and (max-width: 750px) {
-  .public-cloud-content {
-    margin-top: -3.2em !important;
-  }
-}
-
-#home-multi p {
-    color:#eee;
-    line-height:1.6em;
-}
-#home-multi img {
-  margin: 0 auto;
-}
-#home-multi .btn-vmt {
-  margin-top:1em;
-}
-.mktoInline .modal-dialog {
-  margin-top:100px !important;
-}
-.public-cloud-content h4 {
-  font-weight: 600;
-  color:white;
-  font-size: 1.3em;
-}
-.overall-content{
-  padding-top: 5em;
-}
-.list{
-  margin-top:2.5em;
-}
-ol{
-  color: #fff;
-  text-align: left;
-  margin-left: -2em;
-  line-height: 1.8em;
-}
-li {
-  padding-left:0.3em;
-}
-.sub-bullets{
-  list-style-type: disc;
-  margin-left: -1.5em;
-}
-
-</style>
-
-<div id="home-multi" class="container">
-
-  <div class="row">
-    <div class="col-sm-12">
-      <img src="http://localhost:8888/vmturbo/wp-content/uploads/2016/05/pcf-guide.png" class="img-responsive" alt="Public Cloud Guide" title="Public Cloud Guide" style="margin: 0 auto;" />
-    </div>
-  </div>
-  <div class="row overall-content">
-    <div class="col-sm-6">
-      <img src="http://localhost:8888/vmturbo/wp-content/uploads/2016/05/853-ipad-resized-new-cover.png" class="img-responsive hidden-xs" />
-    </div>
-    <div class="col-sm-6">
-
-    <div class="public-cloud-content">
-    <?php the_content(); ?>
-    </div>
+	    <div class="public-cloud-content">
+	    	<?php the_content(); ?>
+	    </div>
     </div>
   </div>
 </div>
